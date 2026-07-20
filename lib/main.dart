14addf97a515d919e694e2c79512d5d4946a8bf5
ahmed.elnoby <ahmed.ellnoby13@gmail.com>
2026-07-20 18:41:11 +0300

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,8 @@ void main() {
 }
 
 class Events extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        HomeScreen.routeName :(context) => HomeScreen(),
-      },
-      initialRoute: HomeScreen.routeName,
-    );
+    return MaterialApp();
   }
 }
-
