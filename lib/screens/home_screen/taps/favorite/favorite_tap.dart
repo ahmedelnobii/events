cfeeee6d5ae791182ff0d7023505bbd79ee4879b
ahmed.elnoby <ahmed.ellnoby13@gmail.com>
2026-07-27 14:07:24 +0300

    implement fav tap ui

--- lib/screens/home_screen/taps/favorite/favorite_tap.dart
+++ lib/screens/home_screen/taps/favorite/favorite_tap.dart
@@ -1,8 +1,31 @@
+import 'package:events/screens/home_screen/widgets/event_item.dart';
+import 'package:events/screens/widgets/custom_text_form_fieled.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteTap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Center(child: Text('Favorite Tap')));
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+            child: CustomTextFormField(
+              hint: 'Search for event',
+              suffix: Icon(CupertinoIcons.search),
+              suffixColor: Theme.of(context).primaryColor,
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              separatorBuilder: (context, index) => SizedBox(height: 16),
+              itemCount: 10,
+              itemBuilder: (_, _) => EventItem(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/widgets/custom_text_form_fieled.dart
+++ lib/screens/widgets/custom_text_form_fieled.dart
@@ -6,18 +6,30 @@ class CustomTextFormField extends StatelessWidget {
   String hint;
   Icon? suffix;
   Icon? prefix;
-  CustomTextFormField({required this.hint, this.suffix, this.prefix});
+  Color? prefixColor;
+  Color? suffixColor;
+  CustomTextFormField({
+    required this.hint,
+    this.suffix,
+    this.prefix,
+    this.suffixColor,
+    this.prefixColor,
+  });
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onTapOutside: (_) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
       cursorColor: Theme.of(context).primaryColor,
       decoration: InputDecoration(
+        filled: true,
         fillColor: AppColors.lightInputField,
-        hint: Text(hint),
+        hint: Text(hint, style: Theme.of(context).textTheme.titleSmall),
         suffixIcon: suffix,
         prefixIcon: prefix,
-        prefixIconColor: AppColors.lightHintText,
-        suffixIconColor: AppColors.lightHintText,
+        prefixIconColor: prefixColor ?? AppColors.lightHintText,
+        suffixIconColor: suffixColor ?? AppColors.lightHintText,
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(16),
           borderSide: BorderSide(color: AppColors.lightOutLinePorder),
